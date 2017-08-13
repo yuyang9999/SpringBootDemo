703630e8a6d1f8086365f38071ce776247721350
yuyang <02dzxxyy@163.com>
2017-08-13 17:23:24 +0800

    secure server test

---
+++ src/main/java/hello/Application.java
@@ -0,0 +1,19 @@
+package hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
+
+/**
+ * Created by yangyu on 13/8/17.
+ */
+
+
+@ComponentScan
+@EnableAutoConfiguration
+public class Application {
+    public static void main(String[] args) throws Throwable {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ src/main/java/hello/MvcConfig.java
@@ -0,0 +1,21 @@
+package hello;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by yangyu on 13/8/17.
+ */
+@Configuration
+public class MvcConfig extends WebMvcConfigurerAdapter {
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/home").setViewName("home");
+        registry.addViewController("/").setViewName("home");
+        registry.addViewController("/hello").setViewName("hello");
+        registry.addViewController("/login").setViewName("login");
+    }
+
+}
