135d0aaaa647fba370542a23450004bc5940840e
yuyang <02dzxxyy@163.com>
2017-08-26 10:24:59 +0800

    add session support

--- src/main/java/hello/RegisterController.java
+++ src/main/java/hello/RegisterController.java
@@ -2,35 +2,55 @@ package hello;
 
 import hello.models.UserAccount;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
-import java.net.URLEncoder;
+import java.util.Map;
 
 /**
  * Created by yangyu on 23/8/17.
  */
 @Controller
+@RequestMapping("user/")
+@SessionAttributes("userId")
 public class RegisterController {
 
-    @RequestMapping(name = "/register", method = RequestMethod.GET)
+    @RequestMapping(value = "/register", method = RequestMethod.GET)
     public String showRegisterForm() {
         return "register";
     }
 
-    @RequestMapping(name = "/register", method=RequestMethod.POST)
-    public String processRegisteration(@Valid UserAccount user, Errors errors) throws  Exception {
+    @RequestMapping(value = "/register", method=RequestMethod.POST)
+    public String processRegisteration(@Valid UserAccount user, Errors errors, Model model) throws  Exception {
         if (errors.hasErrors()) {
             return "register";
         }
 
+        model.addAttribute("userId", 3);
+
         System.out.println(user.getEmail());
         System.out.printf(user.getPassword());
 
-        return "redirect:/greeting?name=" + URLEncoder.encode(user.getEmail(), "UTF-8");
+        return "redirect:/user/check";
     }
 
+    @RequestMapping(value = "/check", method = RequestMethod.GET)
+    public String checkRegisteration(Model model, HttpServletRequest request, HttpSession session) {
+        Map modelMap = model.asMap();
+        for (Object modelKey: modelMap.keySet()) {
+            Object modelValue = modelMap.get(modelKey);
+            System.out.println(modelKey + " -- " + modelValue);
+        }
+
+        model.addAttribute("name", "world");
+
+        return "greeting";
+    }
 
 }
--- src/main/java/hello/models/UserAccount.java
+++ src/main/java/hello/models/UserAccount.java
@@ -28,4 +28,8 @@ public class UserAccount {
         this.email = email;
         this.password = password;
     }
+
+    public String getEmail() {return email;}
+
+    public String getPassword() {return password;}
 }
