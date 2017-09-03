312162ca032fdc1ed1f3267c68571ea1dea803f7
yuyang <02dzxxyy@163.com>
2017-09-03 21:41:31 +0800

    html stuff

--- src/main/java/hello/apis/APIGetProfiles.java
+++ src/main/java/hello/apis/APIGetProfiles.java
@@ -1,8 +1,12 @@
 package hello.apis;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpSession;
+import javax.sql.DataSource;
 
 /**
  * Created by yangyu on 3/9/17.
@@ -10,12 +14,25 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class APIGetProfiles {
 
+    @Autowired
+    DataSource dataSource;
+
     private static final String apiVersion = "/api";
 
     @RequestMapping(apiVersion + "/profiles")
-    public String[] getProfiles() {
+    public String[] getProfiles(HttpSession session) {
         String[] ret =  new String[]{"technology", "finance"};
         return ret;
+    }
+
+    @RequestMapping(apiVersion + "/profile_delete")
+    public boolean deleteOneProfile(@RequestParam(value = "pname") String pname) {
+        return true;
+    }
+
+    @RequestMapping(apiVersion + "/profile_add")
+    public boolean addOneProfile(@RequestParam(value = "pname") String pname) {
+        return true;
     }
 
     @RequestMapping(apiVersion + "/profile_symbols")
@@ -23,9 +40,13 @@ public class APIGetProfiles {
         return new String[]{"AAPL", "NTES"};
     }
 
-    @RequestMapping(apiVersion + "/profile_add")
+    @RequestMapping(apiVersion + "/profile_symbol_add")
     public boolean addProfileSymbol(@RequestParam(value = "pname") String pname,  @RequestParam(value = "sname") String sname) {
         return true;
     }
 
+    @RequestMapping(apiVersion + "/profile_symbol_delete")
+    public boolean deleteProfileSymbol(@RequestParam(value = "pname") String pname, @RequestParam(value = "sname") String sname) {
+        return true;
+    }
 }
