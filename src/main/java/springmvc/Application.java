bbbadad26f10eadf55e9e0ba3e03fa4240257e23
yuyang <02dzxxyy@163.com>
2017-09-19 07:45:20 +0800

    add oauth 2.0

---
+++ src/main/java/springmvc/Application.java
@@ -0,0 +1,15 @@
+package hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by yangyu on 13/8/17.
+ */
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) throws Throwable {
+        SpringApplication.run(Application.class, args);
+    }
+}
